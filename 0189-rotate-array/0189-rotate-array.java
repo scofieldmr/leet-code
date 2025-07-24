20e7372f36e7b7ad56e67988b0942fca9afe24cc
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-07-24 16:24:06 +0530

    Time: 0 ms (100%), Space: 57.9 MB (7.72%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,11 +4,6 @@ class Solution {
         int len = nums.length-1;
         k = k%nums.length;
 
-        if(nums.length<2){
-            System.out.println(Arrays.toString(nums));
-            return;
-        }
-
         rotateArray(nums,0,len);
 
         rotateArray(nums,0,k-1);
@@ -18,7 +13,7 @@ class Solution {
         
     }
 
-     static void rotateArray(int [] arr, int start, int end){
+     static int [] rotateArray(int [] arr, int start, int end){
        
        while(start<end){
           int temp = arr[start];
@@ -28,7 +23,7 @@ class Solution {
           end--;
        }
 
-      System.out.println(Arrays.toString(arr));
+      return arr;
        
     }
 
