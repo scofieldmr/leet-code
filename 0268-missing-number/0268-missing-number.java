c7e0d9bb37716fe6a3d18499831f1edaa3c6d307
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-07-24 15:44:50 +0530

    Time: 1 ms (35.02%), Space: 45.5 MB (30.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,45 +1,25 @@
 class Solution {
     public int missingNumber(int[] arr) {
 
-
-        for(int i=0;i<arr.length;i++){
-            for(int j=i;j<arr.length;j++){
-                if(arr[j]==i){
-                    swap(arr,j,i);
-                    break;
-                }
+        int i=0;
+        while(i<arr.length){
+            int corectIndex = arr[i];
+            if(arr[i]<arr.length && arr[i]!=arr[corectIndex]){
+                swap(arr,i,corectIndex);
+            }
+            else{
+                i++;
             }
         }
-        
-        
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=i){
-                return i;
+
+        for(int in=0;in<arr.length;in++){
+            if(arr[in]!=in){
+                int missNum = in;
+                return missNum;
             }
         }
 
         return arr.length;
-
-
-        // int i=0;
-        // while(i<arr.length){
-        //     int corectIndex = arr[i];
-        //     if(arr[i]<arr.length && arr[i]!=arr[corectIndex]){
-        //         swap(arr,i,corectIndex);
-        //     }
-        //     else{
-        //         i++;
-        //     }
-        // }
-
-        // for(int in=0;in<arr.length;in++){
-        //     if(arr[in]!=in){
-        //         int missNum = in;
-        //         return missNum;
-        //     }
-        // }
-
-        // return arr.length;
         
     }
 
@@ -49,3 +29,23 @@ class Solution {
         arr[sec] = temp;
     }
 }
+
+//Using for loop
+
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=i;j<arr.length;j++){
+        //         if(arr[j]==i){
+        //             swap(arr,j,i);
+        //             break;
+        //         }
+        //     }
+        // }
+        
+        
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]!=i){
+        //         return i;
+        //     }
+        // }
+
+        // return arr.length;
