b42c1e32170dc1d4c1dd0f7f4e998b38ea0f1274
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-07-24 22:03:47 +0530

    Time: 2 ms (80.32%), Space: 46.1 MB (69.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,56 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 
-        int index = 0 ;
+      int k = 0;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-              nums[index]=nums[i];
-              index++;
-            }
+      for(int i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            int temp = nums[i];
+            nums[i] = nums[k];
+            nums[k] = temp;
+            k++;
         }
-
-        for(int i=index;i<nums.length;i++){
-            nums[i] =0;
-        }
-
-
-
-        // int j = 0;
-
-        // int n = nums.length;
-
-        // int[] arr = new int[n];
-
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==0){
-        //         arr[n-1]=nums[i];
-        //     }
-        //     else{
-        //         arr[j] = nums[i];
-        //         j++;
-        //     }
-        // }
-
-        // for(int i=0;i<nums.length;i++){
-        //     nums[i]=arr[i];
-        // }
+      }
         
-        //  for(int i=0;i<nums.length;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i]==0 && nums[j]!=0){
-        //             swap(nums,i,j);
-        //             break;
-        //         }
-        //     }
-        // }
-
-        
-    }
-
-    public void swap(int [] arr,int first,int sec){
-            int temp = arr[first];
-            arr[first] = arr[sec];
-            arr[sec] = temp;
     }
 }
