7d9d36e98642bc1aa9b0138e9215207091666c1c
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-08-07 14:51:26 +0530

    Time: 1 ms (34.6%), Space: 45 MB (93.99%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,51 +1,19 @@
 class Solution {
-    public int missingNumber(int[] arr) {
+    public int missingNumber(int[] nums) {
 
-        int i=0;
-        while(i<arr.length){
-            int corectIndex = arr[i];
-            if(arr[i]<arr.length && arr[i]!=arr[corectIndex]){
-                swap(arr,i,corectIndex);
-            }
-            else{
-                i++;
-            }
+        int[] nums1 = new int[nums.length+1];
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = nums[i];
+            nums1[index] = 1;
         }
 
-        for(int in=0;in<arr.length;in++){
-            if(arr[in]!=in){
-                int missNum = in;
-                return missNum;
+        for (int i = 1; i < nums1.length; i++) {
+            if(nums1[i] == 0){
+                return i;
             }
         }
-
-        return arr.length;
         
-    }
-
-    void swap(int[] arr,int first,int sec){
-        int temp = arr[first];
-        arr[first] = arr[sec];
-        arr[sec] = temp;
+        return 0;
     }
 }
-
-//Using for loop
-
-        // for(int i=0;i<arr.length;i++){
-        //     for(int j=i;j<arr.length;j++){
-        //         if(arr[j]==i){
-        //             swap(arr,j,i);
-        //             break;
-        //         }
-        //     }
-        // }
-        
-        
-        // for(int i=0;i<arr.length;i++){
-        //     if(arr[i]!=i){
-        //         return i;
-        //     }
-        // }
-
-        // return arr.length;
