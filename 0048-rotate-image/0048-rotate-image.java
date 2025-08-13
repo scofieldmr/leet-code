6a951ae452a74d87cfe68c0bc75a637893bfa6d5
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-08-13 12:09:38 +0530

    Time: 0 ms (100%), Space: 42.3 MB (58.98%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,27 +1,22 @@
 class Solution {
     public void rotate(int[][] matrix) {
             
-         int[][] trp = new int[matrix.length][matrix[0].length];
-
-         for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                trp[i][j] = matrix[j][i];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i+1; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
-         for(int [] row : trp){
-             for(int j=0; j<(row.length+1)/2; j++) {
+         for(int [] row : matrix){
+             for(int j=0; j<(matrix.length+1)/2; j++) {
                  int temp = row[j];
                  row[j] = row[row.length-j-1];
-                 row[row.length-j-1] = temp;
+                 row[matrix.length-j-1] = temp;
              }
          }
-          
-         for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = trp[i][j];
-            }
-        }
+        
         
     }
 }
