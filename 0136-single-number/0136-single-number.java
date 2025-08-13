a6153db159536cd76762589c634047c451f4f02a
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-08-13 12:31:37 +0530

    Time: 1 ms (99.84%), Space: 46 MB (72.71%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int singleNum = 0;
+        for(int n:nums){
+            singleNum = singleNum ^ n;
+        }
+
+         return singleNum;
+    }
+}
