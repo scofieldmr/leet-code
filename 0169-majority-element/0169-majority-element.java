ac9b7165b57432034d500ad203ab14bb5821ecbd
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-08-15 20:06:02 +0530

    Time: 1 ms (99.87%), Space: 53.2 MB (49.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,52 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0) + 1);
-        }
-        int max = Integer.MIN_VALUE;
-        int maxKey = Integer.MIN_VALUE;
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            if(entry.getValue() > max){
-                max = entry.getValue();
-                maxKey = entry.getKey();
+        int element=0;
+        int count=0;
+
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]==element){
+                count++;
+            } else if (count==0) {
+                element=nums[i];
+            } else{
+                count--;
             }
         }
 
-        return maxKey;
+        return element;
+
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for(int num : nums){
+        //     map.put(num,map.getOrDefault(num,0) + 1);
+        // }
+        // int max = Integer.MIN_VALUE;
+        // int maxKey = Integer.MIN_VALUE;
+        // for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+        //     if(entry.getValue() > max){
+        //         max = entry.getValue();
+        //         maxKey = entry.getKey();
+        //     }
+        // }
+
+        // return maxKey;
         
     }
 }
+
+//    int maxCount = Integer.MIN_VALUE;
+//         int maxCountValue = Integer.MIN_VALUE;
+//         for(int i=0;i< nums.length;i++){
+//             int count=0;
+//              for(int j=i;j<nums.length;j++){
+//                  if(nums[i]==nums[j]){
+//                      count++;
+//                  }
+//              }
+//              if(count>maxCount){
+//                  maxCount=count;
+//                  maxCountValue=nums[i];
+//              }
+//         }
+
+//         return maxCountValue;
