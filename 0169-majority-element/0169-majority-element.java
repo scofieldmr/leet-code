8a316cda78f9fbf360160137cb239f98fcf90df6
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-08-15 19:53:33 +0530

    Time: 12 ms (36.68%), Space: 49.5 MB (86.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,48 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-    
-
-        int dup = nums[0];
-        int count = 0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==dup){
-                count++;
-                continue;
-            }
-            if(nums[i]!=dup){
-                if(count==0){
-                    dup=nums[i];
-                }
-                else{
-                    count--;
-                }
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0) + 1);
+        }
+        int max = Integer.MIN_VALUE;
+        int maxKey = Integer.MIN_VALUE;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() > max){
+                max = entry.getValue();
+                maxKey = entry.getKey();
             }
         }
 
-        return dup;
-
-
-        //Using Map
-        // Map<Integer,Integer> map = new HashMap<>();
-
-        // for(int i:nums){
-        //     if(map.containsKey(i)){
-        //         map.put(i,(map.get(i))+1);
-        //     }
-        //     else{
-        //         map.put(i,1);
-        //     }
-        // }
-        // int max = Integer.MIN_VALUE;
-        // int maxKey = 0;
-        // for(Map.Entry<Integer,Integer> m: map.entrySet()){
-        //     if(m.getValue()>max){
-        //         max = m.getValue();
-        //         maxKey = m.getKey();
-        //     }
-        // }
-
-        // return maxKey;
+        return maxKey;
+        
     }
 }
