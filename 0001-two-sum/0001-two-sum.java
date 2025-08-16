b14e9396e3f9bfa57b11938ad3517e4f2fd0fd93
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-08-16 22:32:03 +0530

    Time: 2 ms (98.95%), Space: 45.6 MB (5.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -15,6 +15,7 @@ class Solution {
            map.put(nums[i],i);
         }
         return range;
+
         // int [] range = {-1,-1};
         // for(int i=0;i<nums.length;i++){
         //     for(int j=i+1;j<nums.length;j++){
