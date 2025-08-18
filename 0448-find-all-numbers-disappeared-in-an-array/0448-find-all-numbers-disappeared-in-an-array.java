89be34340ded2ceafc455939b2f5bdc3c1052aae
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-08-18 17:34:39 +0530

    Time: 4 ms (95.25%), Space: 56.3 MB (88.99%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,31 +1,49 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
 
-        int index = 0;
+        int [] nums1 = new int [nums.length+1];
 
-        while(index < nums.length) {
-            int correctIndex = nums[index]-1;
-
-            if(nums[index]!=nums[correctIndex]) {
-                int temp = nums[index];
-                nums[index] = nums[correctIndex];
-                nums[correctIndex] = temp;
-            }
-            else {
-                index++;
-            }
+        for(int i=0;i<nums.length;i++){
+            int correctIndex = nums[i];
+            nums1[correctIndex] = nums[i];
         }
 
         List<Integer> list = new ArrayList<Integer>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int correctValue = i+1;
-            if(nums[i]!=correctValue) {
-                list.add(correctValue);
+        for (int i = 1; i < nums1.length; i++) {
+            if(nums1[i]!=i) {
+                list.add(i);
             }
         }
 
         return list;
+
+
+        // int index = 0;
+
+        // while(index < nums.length) {
+        //     int correctIndex = nums[index]-1;
+
+        //     if(nums[index]!=nums[correctIndex]) {
+        //         int temp = nums[index];
+        //         nums[index] = nums[correctIndex];
+        //         nums[correctIndex] = temp;
+        //     }
+        //     else {
+        //         index++;
+        //     }
+        // }
+
+        // List<Integer> list = new ArrayList<Integer>();
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     int correctValue = i+1;
+        //     if(nums[i]!=correctValue) {
+        //         list.add(correctValue);
+        //     }
+        // }
+
+        // return list;
         
     }
 }
