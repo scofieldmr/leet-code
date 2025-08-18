e37f0f831f65d83e216a6f4f2969413bb750c1d5
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-08-18 17:11:55 +0530

    Time: 0 ms (100%), Space: 45.2 MB (71.43%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -15,6 +15,30 @@ class Solution {
         }
 
         return 0;
+
+    //     int index = 0;
+
+    //     while (index < arr.length) {
+    //         int correctIndex = arr[index];
+
+    //         if(arr[index] < arr.length && arr[index] != arr[correctIndex]){
+    //             int temp = arr[index];
+    //             arr[index] = arr[correctIndex];
+    //             arr[correctIndex] = temp;
+    //         }
+    //         else{
+    //             index++;
+    //         }
+    //     }
+
+    //    for (int i = 0; i < arr.length; i++) {
+    //        if(arr[i] != i){
+    //            return i;
+    //        }
+    //    }
+
+    //    return arr.length;
+
         
     }
 }
