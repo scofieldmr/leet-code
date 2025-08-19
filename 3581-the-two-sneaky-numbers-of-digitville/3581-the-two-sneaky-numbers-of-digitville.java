f16f3b498a4e995d44dd79e1ac6c18cbefb4b962
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-08-19 19:48:32 +0530

    Time: 1 ms (100%), Space: 44.5 MB (90.06%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,16 +1,43 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int[] numsCopy = new int[2];
-        int j=0;
-        for(int i = 0; i<nums.length; i++){
-            if(!set.add(nums[i])){
-                numsCopy[j] = nums[i];
+
+        int index = 0;
+
+       while (index < nums.length) {
+           int correctIndex = nums[index];
+           if(nums[correctIndex]!=nums[index]){
+               int temp = nums[correctIndex];
+               nums[correctIndex] = nums[index];
+               nums[index] = temp;
+           }
+           else{
+               index++;
+           }
+       }
+       
+       int[] nums1 = new int[2];
+       int j=0;
+       for(int i = 0; i<nums.length; i++){
+            if(i!=nums[i]){
+                nums1[j] = nums[i];
                 j++;
             }
         }
 
-        return numsCopy;
+        return nums1;
+
+
+        // Set<Integer> set = new HashSet<>();
+        // int[] numsCopy = new int[2];
+        // int j=0;
+        // for(int i = 0; i<nums.length; i++){
+        //     if(!set.add(nums[i])){
+        //         numsCopy[j] = nums[i];
+        //         j++;
+        //     }
+        // }
+
+        // return numsCopy;
         
     }
 }
