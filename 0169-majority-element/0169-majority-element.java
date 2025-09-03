1f8828e3b87c3895b47d49ecb79a58d70abef9f9
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-03 19:58:21 +0530

    Time: 1 ms (99.83%), Space: 53.3 MB (29.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,52 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        int element=0;
-        int count=0;
-
-        for (int i=0;i<nums.length;i++){
-            if (nums[i]==element){
+        int num = nums[0];
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
                 count++;
-            } else if (count==0) {
-                element=nums[i];
-            } else{
+            }
+            else if (count == 0) {
+                num = nums[i];
+            }
+            else{
                 count--;
             }
         }
 
-        return element;
-
-        // Map<Integer,Integer> map = new HashMap<>();
-        // for(int num : nums){
-        //     map.put(num,map.getOrDefault(num,0) + 1);
-        // }
-        // int max = Integer.MIN_VALUE;
-        // int maxKey = Integer.MIN_VALUE;
-        // for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-        //     if(entry.getValue() > max){
-        //         max = entry.getValue();
-        //         maxKey = entry.getKey();
-        //     }
-        // }
-
-        // return maxKey;
-        
+        return num;
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
     }
 }
-
-//    int maxCount = Integer.MIN_VALUE;
-//         int maxCountValue = Integer.MIN_VALUE;
-//         for(int i=0;i< nums.length;i++){
-//             int count=0;
-//              for(int j=i;j<nums.length;j++){
-//                  if(nums[i]==nums[j]){
-//                      count++;
-//                  }
-//              }
-//              if(count>maxCount){
-//                  maxCount=count;
-//                  maxCountValue=nums[i];
-//              }
-//         }
-
-//         return maxCountValue;
