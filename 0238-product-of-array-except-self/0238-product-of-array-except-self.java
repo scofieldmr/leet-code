bca557b208c8f507ab7721c80aef36fbe7f7a449
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-04 11:03:41 +0530

    Time: 1 ms (99.72%), Space: 55.7 MB (41.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,38 +1,37 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
-        int n = nums.length;
-        int[] answer = new int[n];
-
+        int [] prefixArray = new int[nums.length];
         int prefix = 1;
-        for(int i=0;i<nums.length;i++){
-            answer[i]=prefix;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixArray[i]=prefix;
             prefix = prefix * nums[i];
         }
 
+        int [] productArray = new int[nums.length];
         int suffix = 1;
-        for(int i=n-1;i>=0;i--){
-            answer[i]=answer[i]*suffix;
-            suffix = suffix*nums[i];
+
+        for(int i=nums.length-1;i>=0;i--){
+            productArray[i]=prefixArray[i]*suffix;
+            suffix = suffix * nums[i];
         }
 
-        return answer;
+        return productArray;
 
-       
-    }
-}
 
-        // int [] newNums = new int[nums.length];
-    
-        // for(int i=0;i<nums.length;i++){
-        //     int tempProd = 1;
-        //     for(int j=0;j<nums.length;j++){
-        //         if(i==j){
-        //           continue;
+        // int[] productArray = new int[nums.length];
+        // for(int i = 0; i<nums.length; i++){
+        //     int currentProduct = 1;
+        //     for(int j = 0; j<nums.length; j++){
+        //         if(i!=j){
+        //             currentProduct *= nums[j];
         //         }
-        //         tempProd = tempProd*nums[j];
         //     }
-        //     newNums[i] = tempProd;
+        //     productArray[i] = currentProduct;
         // }
 
-        // return newNums;
+        // return productArray;
+        
+    }
+}
