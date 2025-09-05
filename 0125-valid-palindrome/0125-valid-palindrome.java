0cb1736cf728b40cfc004c7a52ed1cd65883edf1
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-05 10:59:12 +0530

    Time: 4 ms (56.27%), Space: 43.2 MB (64.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,62 +1,88 @@
 class Solution {
+
     public boolean isPalindrome(String s) {
+       
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(Character.isLetterOrDigit(c)){
+                if(Character.isDigit(c)){
+                    sb.append(c);
+                }
+                else{
+                    sb.append(Character.toLowerCase(c));
+                }
+            }
+        }
 
-        StringBuilder sb = new StringBuilder(s);
-
-        int i =0;
+        int i = 0;
         int j =sb.length()-1;
 
         while(i<j){
-           char ch1 = Character.toLowerCase(sb.charAt(i));
-           char ch2 = Character.toLowerCase(sb.charAt(j));
-
-           if(Character.isLetter(ch1) || Character.isDigit(ch1)){
-              if(Character.isLetter(ch2) || Character.isDigit(ch2)) {
-                 if (ch1 != ch2) {
-                     return false;
-                 }
-                 else {
-                     System.out.println(ch1 + " = " + ch2);
-                     i++;
-                     j--;
-                     continue;
-                 }
-              }
-              else{
-                  j--;
-              }
-           }
-           else {
-               i++;
-           }
-
+            if(sb.charAt(i)!=sb.charAt(j)){
+                return false;
+            }
+            else {
+                i++;
+                j--;
+            }
         }
 
-        return true;
+       return true;
+    }
 
-    //     StringBuilder sb = new StringBuilder();
-    //     for(int i=0;i<s.length();i++){
-    //         char c = s.toLowerCase().charAt(i);
-    //         if(Character.isLetter(c)  || Character.isDigit(c)){
-    //             sb.append(c);
-    //         }
-    //     }
+    private String convertToString(String s) {
+        StringBuilder sb = new StringBuilder();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                if (Character.isDigit(c)) {
+                    sb.append(c);
+                }
+                else {
+                    sb.append(Character.toLowerCase(c));
+                }
+            }
+        }
+        return sb.toString();
+    }
 
-    //     int i = 0;
-    //     int j =sb.length()-1;
+    
+}
 
-    //     while(i<j){
-    //         if(sb.charAt(i)!=sb.charAt(j)){
-    //             return false;
-    //         }
-    //         else {
-    //             i++;
-    //             j--;
-    //         }
-    //     }
+        // StringBuilder sb = new StringBuilder(s);
 
-    //    return true;
+        // int i =0;
+        // int j =sb.length()-1;
+
+        // while(i<j){
+        //    char ch1 = Character.toLowerCase(sb.charAt(i));
+        //    char ch2 = Character.toLowerCase(sb.charAt(j));
+
+        //    if(Character.isLetter(ch1) || Character.isDigit(ch1)){
+        //       if(Character.isLetter(ch2) || Character.isDigit(ch2)) {
+        //          if (ch1 != ch2) {
+        //              return false;
+        //          }
+        //          else {
+        //              System.out.println(ch1 + " = " + ch2);
+        //              i++;
+        //              j--;
+        //              continue;
+        //          }
+        //       }
+        //       else{
+        //           j--;
+        //       }
+        //    }
+        //    else {
+        //        i++;
+        //    }
+
+        // }
+
+        // return true;
+
 
         
-    }
-}
