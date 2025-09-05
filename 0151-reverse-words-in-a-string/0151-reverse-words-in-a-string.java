8b17e7b0f59458a89c1b4e0c18012c779aa1a632
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-05 20:43:28 +0530

    Time: 6 ms (85.51%), Space: 44.4 MB (23.89%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,28 +1,44 @@
 class Solution {
     public String reverseWords(String s) {
 
-        String [] words = s.split(" ");
+         String [] words = s.split(" ");
 
-
-        List<String> wordList = new ArrayList<>();
-        StringBuilder reversedString = new StringBuilder();
-        for (String word : words) {
-            if(word.length()>0){
-                wordList.add(word);
-            }
-        }
-        System.out.println(wordList);
-
-        for(int i = wordList.size(); i >0; i--){
-            if(i==1){
-                reversedString.append(wordList.get(i-1));
-            }
-            else {
-                reversedString.append(wordList.get(i-1) + " ");
+        StringBuilder builder = new StringBuilder();
+        for (int i = words.length-1; i>=0; i--) {
+            if(words[i].length() > 0){
+                if(i==0){
+                    builder.append(words[i]);
+                }
+                else{
+                    builder.append(words[i]+" ");
+                }
             }
         }
 
-        return reversedString.toString();
+        return builder.toString().trim();
+
+        // String [] words = s.split(" ");
+
+
+        // List<String> wordList = new ArrayList<>();
+        // StringBuilder reversedString = new StringBuilder();
+        // for (String word : words) {
+        //     if(word.length()>0){
+        //         wordList.add(word);
+        //     }
+        // }
+        // System.out.println(wordList);
+
+        // for(int i = wordList.size(); i >0; i--){
+        //     if(i==1){
+        //         reversedString.append(wordList.get(i-1));
+        //     }
+        //     else {
+        //         reversedString.append(wordList.get(i-1) + " ");
+        //     }
+        // }
+
+        // return reversedString.toString();
         
     }
 }
