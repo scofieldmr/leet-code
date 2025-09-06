ba75d74853a02377ed8cf1fe5a90c01277c3a2d5
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-06 10:16:53 +0530

    Time: 6 ms (22.43%), Space: 46.7 MB (69.51%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,21 +3,44 @@ class Solution {
         int [] noOf1Bits = new int[n+1];
 
         for (int i = 0; i <= n; i++) {
-            noOf1Bits[i] = countingBits(i);
+            noOf1Bits[i] = countingBits(i,0);
         }
         
         return noOf1Bits;
     }
 
-     static int countingBits(int n) {
-        int count = 0;
-        while (n!=0){
-            int rem = n&1;
-            if(rem==1){
-                count++;
-            }
-            n = n>>1;
+     static int countingBits(int n, int count) {
+
+        if (n == 0)
+            return count;
+        
+       int rem = n & 1;
+
+        if (rem == 1){
+            count++;
         }
-        return count;
+
+        return countingBits(n>>1, count);
       }
 }
+    // public int[] countBits(int n) {
+    //     int [] noOf1Bits = new int[n+1];
+
+    //     for (int i = 0; i <= n; i++) {
+    //         noOf1Bits[i] = countingBits(i);
+    //     }
+        
+    //     return noOf1Bits;
+    // }
+
+    //  static int countingBits(int n) {
+    //     int count = 0;
+    //     while (n!=0){
+    //         int rem = n&1;
+    //         if(rem==1){
+    //             count++;
+    //         }
+    //         n = n>>1;
+    //     }
+    //     return count;
+    //   }
