83a2da62f804a184ddd05c9a4ba622a3fa30b59d
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-07 20:55:43 +0530

    Time: 1 ms (99.7%), Space: 46.3 MB (9.49%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,23 +1,44 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
 
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if(!set.add(nums[i])){
-                set.remove(nums[i]);
+        int xor = 0;
+        for(int num : nums){
+           xor ^= num;
+        }
+
+
+        int diff = xor & (-xor);
+
+        int num1 = 0;
+        int num2 = 0;
+        for(int num : nums){
+            if((num & diff) == 0){
+                num2 = (num2 ^ num);
+            }
+            else{
+                num1 = (num1 ^ num);
             }
         }
 
-        int[] nums2 = new int[set.size()];
-        int i = 0;
+        return new int [] {num1,num2};
 
-        Iterator<Integer> it = set.iterator();
-        while (it.hasNext()) {
-            nums2[i] = it.next();
-            i++;
-        }
+        // Set<Integer> set = new HashSet<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     if(!set.add(nums[i])){
+        //         set.remove(nums[i]);
+        //     }
+        // }
 
-        return nums2;
+        // int[] nums2 = new int[set.size()];
+        // int i = 0;
+
+        // Iterator<Integer> it = set.iterator();
+        // while (it.hasNext()) {
+        //     nums2[i] = it.next();
+        //     i++;
+        // }
+
+        // return nums2;
         
     }
 }
