b9d1e48d83a572b9ee7b9b7778567a2e8be3d600
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-10 20:23:39 +0530

    Time: 0 ms (100%), Space: 42.6 MB (14.88%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,26 +1,43 @@
 class Solution {
     public void rotate(int[][] matrix) {
 
-        int[][] tmatrix = new int[matrix.length][matrix[0].length];
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                tmatrix[i][j] =  matrix[j][i];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i+1; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
-        for(int i = 0; i < tmatrix.length; i++) {
-            for(int j = 0; j < (tmatrix[i].length+1)/2; j++) {
-                int temp = tmatrix[i][j];
-                tmatrix[i][j] = tmatrix[i][tmatrix[i].length-1-j];
-                tmatrix[i][tmatrix[i].length-1-j] = temp;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < (matrix[i].length+1)/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[i].length-1-j];
+                matrix[i][matrix[i].length-1-j] = temp;
             }
         }
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] =  tmatrix[i][j];
-            }
-        }
+// Using 2d space matrix
+        // int[][] tmatrix = new int[matrix.length][matrix[0].length];
+        // for(int i = 0; i < matrix.length; i++) {
+        //     for(int j = 0; j < matrix[i].length; j++) {
+        //         tmatrix[i][j] =  matrix[j][i];
+        //     }
+        // }
+
+        // for(int i = 0; i < tmatrix.length; i++) {
+        //     for(int j = 0; j < (tmatrix[i].length+1)/2; j++) {
+        //         int temp = tmatrix[i][j];
+        //         tmatrix[i][j] = tmatrix[i][tmatrix[i].length-1-j];
+        //         tmatrix[i][tmatrix[i].length-1-j] = temp;
+        //     }
+        // }
+
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j = 0; j < matrix[i].length; j++) {
+        //         matrix[i][j] =  tmatrix[i][j];
+        //     }
+        // }
         
     }
 }
