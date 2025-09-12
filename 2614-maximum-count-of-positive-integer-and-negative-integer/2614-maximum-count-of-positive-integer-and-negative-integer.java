276305e110ad32e0a3f946a340c6a3ae99214abe
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-12 15:03:27 +0530

    Time: 0 ms (100%), Space: 44.9 MB (64.78%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,18 +1,49 @@
 class Solution {
     public int maximumCount(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
         int negCount = 0;
-        int posCount = 0;
+        while(start <= end) {
+            int mid = (start + end) / 2;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0){
-                posCount++;
+            if(nums[mid]<0){
+                negCount = mid+1;
+                start = mid+1;
             }
-            else if(nums[i]<0){
-                negCount++;
+            else {
+                end = mid - 1;
             }
         }
 
-        return posCount>negCount?posCount:negCount;
+        start = 0;
+        end = nums.length - 1;
+        int posCount = 0;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if(nums[mid]>0){
+                posCount = nums.length - mid;
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+
+        return Math.max(posCount,negCount);
+        // int negCount = 0;
+        // int posCount = 0;
+
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]>0){
+        //         posCount++;
+        //     }
+        //     else if(nums[i]<0){
+        //         negCount++;
+        //     }
+        // }
+
+        // return posCount>negCount?posCount:negCount;
         
     }
 }
