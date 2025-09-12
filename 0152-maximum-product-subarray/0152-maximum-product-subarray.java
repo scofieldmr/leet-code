4ed215c1a4658ff59ea1d52121fd46c18b229afb
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-12 13:07:54 +0530

    Time: 1 ms (90.42%), Space: 45.5 MB (7.43%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,20 +1,40 @@
 class Solution {
     public int maxProduct(int[] nums) {
 
-    int n = nums.length;
-    int maxProduct = Integer.MIN_VALUE;
-    int frontProduct = 1;
-    int backProduct = 1;
+        int maxProduct = nums[0];
+        int minSoFar = nums[0];
+        int maxSoFar = nums[0];
 
-    for (int i = 0; i < n; i++) {
-        frontProduct = (frontProduct == 0 ? 1 : frontProduct) * nums[i];
-        backProduct = (backProduct == 0 ? 1 : backProduct) * nums[n - 1 - i];
+        for (int i = 1; i < nums.length; i++) {
+            int currentProduct = nums[i];
 
-        maxProduct = Math.max(maxProduct, Math.max(frontProduct, backProduct));
-        // also check the single element itself
-        maxProduct = Math.max(maxProduct, Math.max(nums[i], nums[n - 1 - i]));
-    }
+            if(currentProduct<0){
+                int temp = maxSoFar;
+                maxSoFar = minSoFar;
+                minSoFar = temp;
+            }
 
-    return maxProduct;
+            maxSoFar = Math.max(currentProduct,maxSoFar * currentProduct);
+            minSoFar = Math.min(currentProduct,minSoFar * currentProduct);
+
+            maxProduct = Math.max(maxProduct,maxSoFar);
+        }
+
+        return maxProduct;
+
+    // int n = nums.length;
+    // int maxProduct = Integer.MIN_VALUE;
+    // int frontProduct = 1;
+    // int backProduct = 1;
+
+    // for (int i = 0; i < n; i++) {
+    //     frontProduct = (frontProduct == 0 ? 1 : frontProduct) * nums[i];
+    //     backProduct = (backProduct == 0 ? 1 : backProduct) * nums[n - 1 - i];
+
+    //     maxProduct = Math.max(maxProduct, Math.max(frontProduct, backProduct));
+    //     maxProduct = Math.max(maxProduct, Math.max(nums[i], nums[n - 1 - i]));
+    // }
+
+    // return maxProduct;
     }
 }
