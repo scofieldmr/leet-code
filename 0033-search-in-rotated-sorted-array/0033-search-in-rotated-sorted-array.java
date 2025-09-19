73d66b677d40a6cbe90b322d7bc82644044dcf2f
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-19 12:23:37 +0530

    Time: 0 ms (100%), Space: 42 MB (84.36%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+
+        int peakIndex = peakIndex(nums);
+
+        if(peakIndex==-1){
+            return search(nums,target,0,nums.length-1);
+        }
+        else if(search(nums,target,0,peakIndex)!=-1){
+            return search(nums,target,0,peakIndex);
+        }
+
+        
+        return search(nums,target,peakIndex+1,nums.length-1);
+        
+    }
+
+     public static int search(int[] nums, int target,int start, int end) {
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int peakIndex(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(mid<end && nums[mid]>nums[mid+1]) {
+                return mid;
+            }
+           
+            if(mid>start && nums[mid-1]>nums[mid]) {
+                return mid-1;
+            }
+
+            if (nums[mid] >= nums[start]) {
+                start = mid + 1; 
+            } else {
+                end = mid - 1; 
+            }
+            
+        }
+        return -1;
+    }
+}
