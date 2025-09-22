f3162739faba65362a9d6129abb15fe35c7c3233
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-22 19:01:22 +0530

    Time: 2 ms (99.06%), Space: 42.3 MB (65.76%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int balance = 0;
+          for(char c : s.toCharArray()) {
+           if(c=='('){
+               if(balance>0){
+                   sb.append(c);
+               }
+               balance++;
+           }
+           else{
+               balance--;
+               if(balance>0){
+                   sb.append(c);
+               }
+           }
+       }
+
+       return sb.toString();
+
+
+    }
+}
