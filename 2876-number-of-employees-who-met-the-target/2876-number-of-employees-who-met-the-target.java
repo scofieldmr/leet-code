e623d989337409ccc84597f82302fb54ae46b177
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-25 14:04:33 +0530

    Time: 0 ms (100%), Space: 42.9 MB (8.74%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+
+        for(int h : hours){
+            if(h>=target){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
