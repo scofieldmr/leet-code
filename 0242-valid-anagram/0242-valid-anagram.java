5b7987da23d7b0010220f7d52674b43a25e6b28a
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-26 21:28:19 +0530

    Time: 4 ms (76.1%), Space: 43.2 MB (71.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+
+        if (s.length()!=t.length()){
+            return false;
+        }
+        
         int[] count = new int[26];
 
         for(int i=0;i<s.length();i++){
