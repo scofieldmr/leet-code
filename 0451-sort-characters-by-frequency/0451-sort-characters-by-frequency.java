2620f6bd3580753c4c1226ebc657cff79d02eccf
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-09-27 11:09:43 +0530

    Time: 16 ms (37.61%), Space: 45.1 MB (89.77%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,30 +1,59 @@
 class Solution {
     public String frequencySort(String s) {
+
         Map<Character, Integer> map1= new HashMap<>();
 
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
             if(map1.containsKey(ch)){
-                    map1.put(ch, map1.get(ch)+1);
-                }
-                else{
-                    map1.put(ch, 1);
-                }
+                   map1.put(ch, map1.get(ch)+1);
+               }
+               else {
+                map1.put(ch, 1);
+            }
         }
+        List<Map.Entry<Character, Integer>> mapList= map1.entrySet().stream()
+                 .sorted(Comparator.comparingInt(Map.Entry::getValue)).collect(Collectors.toList());
 
-        Map<Character,Integer> newmap1 = map1.entrySet().stream().sorted(Map.Entry.<Character,Integer>comparingByValue().reversed())
-                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        StringBuilder builder = new StringBuilder();
 
+        int size=mapList.size()-1;
 
-        StringBuilder stringBuilder = new StringBuilder();
-        for(Map.Entry<Character,Integer> entry: newmap1.entrySet()){
-            int count = entry.getValue();
-            while (count>0){
-                stringBuilder.append(entry.getKey());
+        for(int j=size; j>=0; j--){
+            char ch = mapList.get(j).getKey();
+            int count=mapList.get(j).getValue();
+            while(count>0){
+                builder.append(ch);
                 count--;
             }
         }
 
-        return stringBuilder.toString();
+        return builder.toString();
+        // Map<Character, Integer> map1= new HashMap<>();
+
+        // for(int i=0; i<s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     if(map1.containsKey(ch)){
+        //             map1.put(ch, map1.get(ch)+1);
+        //         }
+        //         else{
+        //             map1.put(ch, 1);
+        //         }
+        // }
+
+        // Map<Character,Integer> newmap1 = map1.entrySet().stream().sorted(Map.Entry.<Character,Integer>comparingByValue().reversed())
+        //                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+
+        // StringBuilder stringBuilder = new StringBuilder();
+        // for(Map.Entry<Character,Integer> entry: newmap1.entrySet()){
+        //     int count = entry.getValue();
+        //     while (count>0){
+        //         stringBuilder.append(entry.getKey());
+        //         count--;
+        //     }
+        // }
+
+        // return stringBuilder.toString();
     }
 }
