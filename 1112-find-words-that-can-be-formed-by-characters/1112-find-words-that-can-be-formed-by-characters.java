7e7c76837c2e2c227b74a1b0f12fbe3e0aee480c
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-18 13:20:43 +0530

    Time: 6 ms (77.17%), Space: 45.1 MB (83.83%) - LeetHub

--- 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.java
@@ -2,27 +2,31 @@ class Solution {
     public int countCharacters(String[] words, String chars) {
 
         int [] c = new int [26];
-        for(int i=0;i<chars.length();i++){
-            int index = chars.charAt(i)-'a';
-            c[index] = c[index] + 1;
+        for(char ch : chars.toCharArray()){
+            c[ch -'a']++;
         }
 
         int count = 0;
 
-        for(int i=0;i<words.length;i++){
-            String st = words[i];
-            int curr = 0;
-            int [] cd = Arrays.copyOf(c,c.length);
-            for(int j=0;j<st.length();j++){
-                int index = st.charAt(j)-'a';
-                if(cd[index]<=0){
-                   break;
-                }
-                cd[index] = cd[index] -1;
-                curr++;
+        
+        for(String w: words){
+            int [] temp = new int[26];
+
+            for(int j=0;j<w.length();j++){
+                temp[w.charAt(j)-'a']++;
             }
-            if(curr == st.length()){
-               count = count + st.length();
+            
+            boolean canForm = true;
+
+            for(int j=0;j<temp.length;j++){
+                if(temp[j]>c[j]){
+                    canForm = false;
+                    break;
+                }
+            }
+
+            if(canForm){
+                count = count + w.length();
             }
         }
 
@@ -30,3 +34,23 @@ class Solution {
         
     }
 }
+
+//Using the array copy method to check
+        // for(String w : words){
+        //     String st = w;
+        //     int curr = 0;
+        //     int [] cd = Arrays.copyOf(c,c.length);
+        //     for(int j=0;j<st.length();j++){
+        //         int index = st.charAt(j)-'a';
+        //         if(cd[index]<=0){
+        //            break;
+        //         }
+        //         cd[index] = cd[index] -1;
+        //         curr++;
+        //     }
+        //     if(curr == st.length()){
+        //        count = count + st.length();
+        //     }
+        // }
+
+        // return count;
