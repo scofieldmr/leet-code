9e1da7288e7e08522551428cb568ecb8ba40a8d3
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-18 13:41:00 +0530

    Time: 0 ms (100%), Space: 41.6 MB (56.79%) - LeetHub

--- 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.java
+++ 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.java
@@ -1,29 +1,48 @@
 class Solution {
     public int rearrangeCharacters(String s, String target) {
+
         int[] scount = new int[26];
         for(char c: s.toCharArray()){
             scount[c - 'a']++;
         }
 
-        int count = 0;
-        for(int i=0;i<s.length();i++){
-            boolean isAvailable = true;
-            for(char c: target.toCharArray()){
-                int index = c - 'a';
-                if(scount[index] == 0){
-                    isAvailable = false;
-                    break;
-                }
-                scount[index]--;
-            }
-            if(isAvailable){
-                count = count+1;
-            }
-            else{
-                break;
-            }
+        int [] tcount = new int[26];
+        for(char c: target.toCharArray()){
+            tcount[c - 'a']++;
         }
 
-        return count;
+        int minCount = Integer.MAX_VALUE;
+        for (char c: target.toCharArray()) {
+           int index = c - 'a';
+           minCount = Math.min(minCount, scount[index]/tcount[index]);
+        }
+
+        return minCount;
+        //using frequency Method
+        // int[] scount = new int[26];
+        // for(char c: s.toCharArray()){
+        //     scount[c - 'a']++;
+        // }
+
+        // int count = 0;
+        // for(int i=0;i<s.length();i++){
+        //     boolean isAvailable = true;
+        //     for(char c: target.toCharArray()){
+        //         int index = c - 'a';
+        //         if(scount[index] == 0){
+        //             isAvailable = false;
+        //             break;
+        //         }
+        //         scount[index]--;
+        //     }
+        //     if(isAvailable){
+        //         count = count+1;
+        //     }
+        //     else{
+        //         break;
+        //     }
+        // }
+
+        // return count;
     }
 }
