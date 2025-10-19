3aa86f7e8654899b61444b4e84d4122e7d33d522
MdBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-19 19:10:57 +0530

    Time: 1 ms (63.06%), Space: 40.7 MB (64.73%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set=new HashSet<>();
+        int sqnum = n;
+        boolean flag = false;
+
+        while (!flag) {
+            sqnum = squareNum(sqnum);
+            if (set.contains(sqnum)) {
+                break;
+            }
+            if(sqnum == 1){
+                flag = true;
+                break;
+            }
+            set.add(sqnum);
+        }
+
+        if(flag){
+            return true;
+        }
+        
+        return false;
+    }
+
+     public static int squareNum(int num) {
+        int squareNum=0;
+        while(num!=0){
+            int rem = num%10;
+            squareNum = squareNum+(rem*rem);
+            num = num/10;
+        }
+        return squareNum;
+    }
+}
