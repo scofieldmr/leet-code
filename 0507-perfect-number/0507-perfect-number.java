921c1f07e8c48eb318684c676391fa53ce444fc3
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-22 22:05:39 +0530

    Time: 2 ms (71.2%), Space: 40.9 MB (13%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        if(num==1){
+        if(num<=1){
             return false;
         }
 
-        int sum = 0;
-        for(int i = 1; i <= Math.sqrt(num); i++) {
+        int sum = 1;
+        for(int i = 2; i <= Math.sqrt(num); i++) {
             if(num % i == 0) {
                 sum += i;
                 if(num/i != num){
