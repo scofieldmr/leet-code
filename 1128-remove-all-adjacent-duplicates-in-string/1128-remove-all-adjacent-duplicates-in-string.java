f2d74d9f8a3ec423afdd7e30b731b794b66afdc6
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-29 17:54:26 +0530

    Time: 10 ms (90.43%), Space: 45.3 MB (85.46%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,24 +1,38 @@
 class Solution {
     public String removeDuplicates(String s) {
-        Stack<String> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+       for(char c : s.toCharArray()){
+           int len = sb.length();
 
-        stack.push(String.valueOf(s.charAt(0)));
-        for (int i = 1; i < s.length(); i++) {
-            if(!stack.isEmpty() && stack.peek().equals(String.valueOf(s.charAt(i)))){
-                stack.pop();
-            }
-            else{
-                stack.push(String.valueOf(s.charAt(i)));
-            }
-        }
+           if(len>0 && sb.charAt(len-1)==c){
+               sb.deleteCharAt(len-1);
+           }
+           else{
+               sb.append(c);
+           }
+       }
+       
+       return sb.toString();
+        //Using stack
+        // Stack<String> stack = new Stack<>();
+
+        // stack.push(String.valueOf(s.charAt(0)));
+        // for (int i = 1; i < s.length(); i++) {
+        //     if(!stack.isEmpty() && stack.peek().equals(String.valueOf(s.charAt(i)))){
+        //         stack.pop();
+        //     }
+        //     else{
+        //         stack.push(String.valueOf(s.charAt(i)));
+        //     }
+        // }
 
 
 
-        StringBuilder stringBuilder = new StringBuilder();
-        while (!stack.isEmpty()){
-            stringBuilder.append(stack.pop());
-        }
+        // StringBuilder stringBuilder = new StringBuilder();
+        // while (!stack.isEmpty()){
+        //     stringBuilder.append(stack.pop());
+        // }
         
-        return stringBuilder.reverse().toString();
+        // return stringBuilder.reverse().toString();
     }
 }
