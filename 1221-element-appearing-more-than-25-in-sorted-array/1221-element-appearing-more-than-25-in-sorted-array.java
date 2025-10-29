8377d7f07c6f7273731091a75bc504753d3a3905
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-29 16:36:08 +0530

    Time: 1 ms (60.04%), Space: 44.8 MB (76.7%) - LeetHub

--- 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.java
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.java
@@ -1,22 +1,53 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int len = arr.length;
-        int len_30 = arr.length/3;
 
-        for(int a:arr){
-            map.put(a,map.getOrDefault(a,0)+1);
+        if(arr.length==1){
+            return arr[0];
         }
 
+        int maxCount = Integer.MIN_VALUE;
         int max = Integer.MIN_VALUE;
-        int mlen = Integer.MIN_VALUE;
-        for(Map.Entry<Integer,Integer> m : map.entrySet()){
-            if(m.getValue()>mlen){
-                mlen = m.getValue(); 
-                max = m.getKey();
+        int curr = arr[0];
+        int currcount = 1;
+        
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==curr){
+                currcount++;
+            }
+            else{
+                if(currcount>maxCount){
+                    max = curr;
+                    maxCount = currcount;
+                }
+                curr = arr[i];
+                currcount = 1;
             }
         }
 
+        if(currcount>maxCount){
+            max = curr;
+        }
+
         return max;
+
+        //using Map
+        // Map<Integer,Integer> map = new HashMap<>();
+        // int len = arr.length;
+        // int len_30 = arr.length/3;
+
+        // for(int a:arr){
+        //     map.put(a,map.getOrDefault(a,0)+1);
+        // }
+
+        // int max = Integer.MIN_VALUE;
+        // int mlen = Integer.MIN_VALUE;
+        // for(Map.Entry<Integer,Integer> m : map.entrySet()){
+        //     if(m.getValue()>mlen){
+        //         mlen = m.getValue(); 
+        //         max = m.getKey();
+        //     }
+        // }
+
+        // return max;
     }
 }
