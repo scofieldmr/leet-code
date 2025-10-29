15cfd8cd4d5b33ab092382b4475ba9ecaf8f2bcd
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-29 18:34:21 +0530

    Time: 0 ms (100%), Space: 45.5 MB (33.27%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,44 +1,29 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int[] newnums = new int [nums.length+1];
 
-        int [] nums1 = new int [nums.length+1];
-        
-        for(int i = 0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){
             int index = nums[i];
-            nums1[index] = nums[i];
+            newnums[index]++;
         }
 
-        for(int i=0;i<nums1.length;i++){
-           if(nums1[i]!=i){
-             return i;
-           }
+        for(int i=0;i<newnums.length;i++){
+            if(newnums[i]==0){
+                return i;
+            }
         }
 
-        return 0;
-
-    //     int index = 0;
-
-    //     while (index < arr.length) {
-    //         int correctIndex = arr[index];
-
-    //         if(arr[index] < arr.length && arr[index] != arr[correctIndex]){
-    //             int temp = arr[index];
-    //             arr[index] = arr[correctIndex];
-    //             arr[correctIndex] = temp;
-    //         }
-    //         else{
-    //             index++;
-    //         }
-    //     }
-
-    //    for (int i = 0; i < arr.length; i++) {
-    //        if(arr[i] != i){
-    //            return i;
-    //        }
-    //    }
-
-    //    return arr.length;
-
+        return newnums.length-1;
+        // Arrays.sort(nums);
         
+        // int i=0;
+        // while(i<nums.length){
+        //     if(i!=nums[i]){
+        //        return i;
+        //     }
+        //     i++;
+        // }
+
+        // return nums.length;
     }
 }
