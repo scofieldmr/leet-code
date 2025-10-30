4b7e05c5c65c6acbc73243e49a14bf2e6173ebf0
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-30 18:46:37 +0530

    Time: 0 ms (100%), Space: 40.8 MB (13.55%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.java
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -1,38 +1,48 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        int frontCount = 0;
-        int backCount = 0;
+ 
+          int cycle = time % (2 * (n - 1));
 
-        int perRound = n - 1;
+          if(cycle<n){
+             return cycle+1;
+          }
+          else{
+            return n - (cycle - (n-1));
+          }
 
-        while(time>perRound){
-            if(frontCount == backCount){
-                time = time - perRound;
-                frontCount++;
-            }
-            else{
-                time = time - perRound;
-                backCount++;
-            }
-        }
+        // int frontCount = 0;
+        // int backCount = 0;
 
-        if(frontCount == backCount) {
-           for(int i=1;i<=n;i++){
-               if(time == 0){
-                   return i;
-               }
-               time--;
-           }
-        }
-        else{
-            for(int i=n;i>=1;i--){
-                if(time == 0){
-                    return i;
-                }
-                time--;
-            }
-        }
+        // int perRound = n - 1;
 
-        return -1;
+        // while(time>perRound){
+        //     if(frontCount == backCount){
+        //         time = time - perRound;
+        //         frontCount++;
+        //     }
+        //     else{
+        //         time = time - perRound;
+        //         backCount++;
+        //     }
+        // }
+
+        // if(frontCount == backCount) {
+        //    for(int i=1;i<=n;i++){
+        //        if(time == 0){
+        //            return i;
+        //        }
+        //        time--;
+        //    }
+        // }
+        // else{
+        //     for(int i=n;i>=1;i--){
+        //         if(time == 0){
+        //             return i;
+        //         }
+        //         time--;
+        //     }
+        // }
+
+        // return -1;
     }
 }
