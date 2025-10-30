2f59cb17b728b50f5ffc926b03eb6650f0fa02d4
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-30 18:05:22 +0530

    Time: 7 ms (94.02%), Space: 45.2 MB (87.16%) - LeetHub

--- 1260-day-of-the-year/1260-day-of-the-year.java
+++ 1260-day-of-the-year/1260-day-of-the-year.java
@@ -6,23 +6,48 @@ class Solution {
         int month = Integer.parseInt(date.substring(5,7));
         int year = Integer.parseInt(date.substring(0,4));
 
+        int [] monthDays = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-        for (int i = 1; i < month; i++) {
-            if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
-                dayOfYear = dayOfYear + 31;
-            }
-            else if(i == 4 || i == 6 || i == 9 || i == 11) {
-                dayOfYear = dayOfYear + 30;
-            }
-            else if(i == 2 && (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        boolean isLeap = false;
+        if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
+            isLeap = true;
+        }
+
+        dayOfYear = monthDate;
+        for(int i = 0; i < month - 1; i++){
+            if(isLeap && i == 1){
                 dayOfYear = dayOfYear + 29;
             }
-            else if(i == 2){
+            else if(i==1){
                 dayOfYear = dayOfYear + 28;
             }
+            else{
+                dayOfYear = dayOfYear + monthDays[i];
+            }
+
+            
         }
-        dayOfYear = dayOfYear + monthDate;
 
         return dayOfYear;
+
+
+
+        // for (int i = 1; i < month; i++) {
+        //     if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
+        //         dayOfYear = dayOfYear + 31;
+        //     }
+        //     else if(i == 4 || i == 6 || i == 9 || i == 11) {
+        //         dayOfYear = dayOfYear + 30;
+        //     }
+        //     else if(i == 2 && (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        //         dayOfYear = dayOfYear + 29;
+        //     }
+        //     else if(i == 2){
+        //         dayOfYear = dayOfYear + 28;
+        //     }
+        // }
+        // dayOfYear = dayOfYear + monthDate;
+
+        // return dayOfYear;
     }
 }
