5c52072e0a5053270ef541e11fef3c54296348d3
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-30 18:40:40 +0530

    Time: 0 ms (100%), Space: 40.6 MB (32.86%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int frontCount = 0;
+        int backCount = 0;
+
+        int perRound = n - 1;
+
+        while(time>perRound){
+            if(frontCount == backCount){
+                time = time - perRound;
+                frontCount++;
+            }
+            else{
+                time = time - perRound;
+                backCount++;
+            }
+        }
+
+        if(frontCount == backCount) {
+           for(int i=1;i<=n;i++){
+               if(time == 0){
+                   return i;
+               }
+               time--;
+           }
+        }
+        else{
+            for(int i=n;i>=1;i--){
+                if(time == 0){
+                    return i;
+                }
+                time--;
+            }
+        }
+
+        return -1;
+    }
+}
