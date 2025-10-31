da10e14f646460902abce14bc081c48a8bf7be1d
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-31 22:14:12 +0530

    Time: 0 ms (100%), Space: 42.7 MB (6.61%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -1,7 +1,23 @@
 class Solution {
     public String toHex(int num) {
+        if(num==0){
+            return "0";
+        }
+        
+        long n = num & 0xFFFFFFFFL;
 
-        return Integer.toHexString(num);
+        char [] ch = "0123456789abcdef".toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+
+        while (n!=0){
+            long rem = n % 16;
+            sb.append(ch[(int)rem]);
+            n /= 16;
+        }
+        return sb.reverse().toString();
+
+        // return Integer.toHexString(num);
         
     }
 }
