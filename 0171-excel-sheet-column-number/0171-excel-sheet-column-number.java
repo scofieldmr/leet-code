c8c59ed08d2b465e76349d4070470aca7674d650
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-10-31 20:43:49 +0530

    Time: 0 ms (100%), Space: 43.5 MB (9.16%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -3,8 +3,7 @@ class Solution {
         int result = 0;
 
         for(int i=0;i<columnTitle.length();i++){
-            int val = columnTitle.charAt(i) - 'A' + 1;
-            result = (result*26)+ val;
+            result = (result*26)+ (columnTitle.charAt(i) - 'A' + 1);
         }
 
         return result;
