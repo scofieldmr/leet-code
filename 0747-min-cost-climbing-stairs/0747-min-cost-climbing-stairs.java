35261a0051e6027805f354a321486499a3da24fd
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-11-01 23:16:31 +0530

    Time: 0 ms (100%), Space: 44.7 MB (5.56%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -5,6 +5,7 @@ class Solution {
 
         for(int i=2;i<cost.length;i++){
             int curr = cost[i] + Math.min(prev1,prev2);
+           
             prev1 = prev2;
             prev2 = curr;
         }
