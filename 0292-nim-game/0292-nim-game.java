5d219e50dd14c421cca8c8b257d4303e94225fc3
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-11-02 22:03:12 +0530

    Time: 0 ms (100%), Space: 42.2 MB (9.95%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if(n%4==0){
-            return false;
+        if(n%4!=0){
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
