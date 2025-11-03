4cdb3ffae7022b9bb7067203df7f28feeecc9f7c
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-11-03 20:45:00 +0530

    Time: 3 ms (82.25%), Space: 47.2 MB (5.07%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,68 +1,54 @@
 class Solution {
-    public int romanToInt(String s) {
-        int count = 0;
-        
-        for(int i=s.length()-1;i>=0;i--){
-            if(s.charAt(i)=='I'){
-                count = count + 1;
+    public int romanToInt(String roman) {
+         int num = 0;
+        for (int i = 0; i < roman.length(); i++) {
+            if(roman.charAt(i) == 'M'){
+                num = num + 1000;
             }
-            else if(s.charAt(i)=='V'){
-                if(i>0 && s.charAt(i-1)=='I'){
-                    count = count+4;
-                    i = i-1;
-                }
-                else{
-                    count = count + 5;
-                }
+            else if((i+1) < roman.length() && roman.charAt(i) == 'C' && roman.charAt(i+1) == 'M'){
+                num = num + 900;
+                i = i+1;
             }
-            else if(s.charAt(i)=='X'){
-                if(i>0 && s.charAt(i-1)=='I'){
-                    count = count+9;
-                    i = i-1;
-                }
-                else{
-                    count = count + 10;
-                }
+            else if(i+1 < roman.length() && roman.charAt(i) == 'C' && roman.charAt(i+1) == 'D'){
+                num = num + 400;
+                i = i+1;
             }
-            else if(s.charAt(i)=='L'){
-                if(i>0 && s.charAt(i-1)=='X'){
-                    count = count+40;
-                    i = i-1;
-                }
-                else{
-                    count = count + 50;
-                }
+            else if(roman.charAt(i) == 'D'){
+                num = num + 500;
             }
-             else if(s.charAt(i)=='C'){
-                if(i>0 && s.charAt(i-1)=='X'){
-                    count = count+ 90;
-                    i = i-1;
-                }
-                else{
-                    count = count + 100;
-                }
+            else if(i+1 < roman.length() && roman.charAt(i) == 'X' && roman.charAt(i+1) == 'C'){
+                num = num + 90;
+                i = i+1;
             }
-            else if(s.charAt(i)=='D'){
-                if(i>0 && s.charAt(i-1)=='C'){
-                    count = count+ 400;
-                    i = i-1;
-                }
-                else{
-                    count = count + 500;
-                }
+            else if(roman.charAt(i) == 'C'){
+                num = num + 100;
             }
-            else if(s.charAt(i)=='M'){
-                if(i>0 && s.charAt(i-1)=='C'){
-                    count = count+ 900;
-                    i = i-1;
-                }
-                else{
-                    count = count + 1000;
-                }
+            else if(i+1 < roman.length() && roman.charAt(i) == 'X' && roman.charAt(i+1) == 'L'){
+                num = num + 40;
+                i = i+1;
+            }
+            else if(roman.charAt(i) == 'L'){
+                num = num + 50;
+            }
+            else if(i+1 < roman.length() && roman.charAt(i) == 'I' && roman.charAt(i+1) == 'X'){
+                num = num + 9;
+                i = i+1;
+            }
+            else if(roman.charAt(i) == 'X'){
+                num = num + 10;
+            }
+            else if(i+1 < roman.length() && roman.charAt(i) == 'I' && roman.charAt(i+1) == 'V'){
+                num = num + 4;
+                i = i+1;
+            }
+            else if(roman.charAt(i) == 'V'){
+                num = num + 5;
+            }
+            else if (roman.charAt(i) == 'I') {
+                num = num + 1;
             }
         }
-        
-        return count;
-    
+
+        return num;
     }
 }
