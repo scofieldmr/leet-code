edd7a07a990e5afd880b61404eaa19566fb6628c
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-11-04 18:07:40 +0530

    Time: 93 ms (85.58%), Space: 51 MB (15.21%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -3,27 +3,27 @@ class Solution {
         if(n<=2){
             return 0;
         }
-        boolean[] primes = new boolean[n + 1];
+        boolean[] primes = new boolean[n];
+        Arrays.fill(primes, true);
 
-        for (int i = 2; i < primes.length; i++) {
-            primes[i] = true;
+        primes[0] = false;
+        primes[1] = false;
+
+        for(int i = 2; i<= Math.sqrt(n); i++) {
+            if(primes[i]) {
+                for(int j=i*i; j<n; j+=i) {
+                    primes[j] = false;
+                }
+            }
         }
 
-       for(int i =2; i<=Math.sqrt(n); i++){
-           if(primes[i]){
-               for (int j = i*i; j <=n; j+=i){
-                   primes[j] = false;
-               }
-           }
-       }
 
-       
-       int count = 0;
-       for (int i = 2; i < n; i++) {
-           if(primes[i]){
-               count++;
-           }
-       }
+        int count = 0;
+        for(boolean prime : primes) {
+            if(prime) {
+                count++;
+            }
+        }
 
        return count;
     }
