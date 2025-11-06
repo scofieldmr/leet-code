0dafeb5a1e37f0ca45a19c98c314f90e4818d141
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-11-06 12:47:47 +0530

    Time: 1 ms (23.23%), Space: 46.4 MB (5.27%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,12 +11,27 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        int length = cycleLength(head);
+        ListNode slow = head;
+        ListNode fast = head;
+        int length = 0;
+
+        while (fast != null && fast.next!= null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                ListNode temp = slow;
+                do{
+                    temp = temp.next;
+                    length++;
+                }while(temp != slow);
+                break;
+            }
+        }
 
         if(length==0){
             return null;
         }
-
+        
         ListNode first = head;
         ListNode second = head;
 
@@ -28,28 +43,33 @@ public class Solution {
         while(first != second){
             first = first.next;
             second = second.next;
-        }
-
-        return second;
-    }
-
-    public static int cycleLength(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        int length = 0;
-        while(fast!=null && fast.next!=null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if(fast == slow){
-                ListNode temp = slow;
-                do{
-                    temp = temp.next;
-                    length++;
-                }while(temp != slow);
-                return length;
+            if(first == second){
+                return first;
             }
         }
-        return 0;
+
+        return head;
     }
+
+    //  public static int cycleLength(ListNode head) {
+    //     if (head == null || head.next == null) {
+    //         return 0;
+    //     }
+    //     ListNode slow = head;
+    //     ListNode fast = head;
+    //     int length = 0;
+    //     while (fast.next != null && fast.next.next != null) {
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //         if (slow == fast) {
+    //             ListNode temp = slow;
+    //             do{
+    //                 temp = temp.next;
+    //                 length++;
+    //             }while(temp != slow);
+    //             return length;
+    //         }
+    //     }
+    //     return 0;
+    // }
 }
