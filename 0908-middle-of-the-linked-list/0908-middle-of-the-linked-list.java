bd0ab3136e42ee08d9ce5f3084c74c7728278c27
MohamedBilal <66351159+scofieldmr@users.noreply.github.com>
2025-11-06 13:07:06 +0530

    Time: 0 ms (100%), Space: 42.8 MB (5.11%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -9,44 +9,15 @@
  * }
  */
 class Solution {
-     public ListNode middleNode(ListNode head) {
-        
+    public ListNode middleNode(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
 
         while(fast!=null && fast.next!=null){
-            slow= slow.next;
+            slow = slow.next;
             fast = fast.next.next;
         }
 
         return slow;
-     }
-     //Using the length of the linked list
-    // public ListNode middleNode(ListNode head) {
-    //     int len = getLength(head);
-
-    //     int mid = (len/2)+1;
-
-    //     ListNode temp = head;
-    //     int currLen = 1;
-
-    //     while(currLen!=mid && temp!=null){
-    //         temp = temp.next;
-    //         currLen++;
-    //     }
-
-    //     return temp;
-    // }
-
-    // public int getLength(ListNode head){
-    //     ListNode curr = head;
-    //     int length = 0;
-
-    //     while(curr!=null){
-    //          length++;
-    //          curr = curr.next;
-    //     }
-
-    //     return length;
-    // }
+    }
 }
